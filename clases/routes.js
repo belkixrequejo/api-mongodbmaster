a0af3a62ba0d2877d422d46853aed7e1cc659d7f
belkixrequejo <belkix.requejo@pucese.edu.ec>
2020-02-06 13:07:59 -0500

    clases routas

--- clases/routes.js
+++ clases/routes.js
@@ -1,7 +1,8 @@
 
 module.exports = (app) => {
     const aulas = require('./aulas.controller');
-
+    const clases = require('./clases.controller.js');
+    //ROUTES AULAS
     // Create a new Materia
     app.post('/aulas', aulas.create);
 
@@ -16,6 +17,22 @@ module.exports = (app) => {
 
     // Delete a Materia with idmateria
     app.get('/aulas/delete/:id', aulas.delete);
+
+
+    // Create a new Clase
+    app.post('/class', clases.create);
+
+    // Retrieve all Materias
+    app.get('/class', clases.findAll);
+
+    // Retrieve a single Materia with idmateria
+    app.get('/class/:id', clases.findOne);
+
+    // Update a Materia with idmateria
+    app.put('/class/update/:id', clases.update);
+
+    // Delete a Materia with idmateria
+    app.delete('/class/delete/:id', clases.delete);
 
 
 }
