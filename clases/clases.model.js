ab90ee521cdcc49eca3b4402c956066a3e874652
belkixrequejo <belkix.requejo@pucese.edu.ec>
2020-02-06 13:13:36 -0500

    clases controller

--- clases/clases.model.js
+++ clases/clases.model.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 const HAulas = require('./aulas.model.js');
+const Husr = require('./usuarios.model');
 
 const haulas = new HAulas();
 const HclasesSchema = mongoose.Schema({
-    idmateria:      String ,
+    id:      String ,
     aulas:          haulas ,
     dia:            String ,
     horainicio:     Date   ,
     horafinal:      Date   ,
     docente:        String ,
     materia:        String ,
+    docente:        String ,
     nivel:          String ,
     carrera:        String
     }, {
