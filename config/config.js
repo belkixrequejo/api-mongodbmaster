a663991d58fae8e9ef7907c4d2baaede76f262f0
JoseCobena <jose.cobena@pucese.edu.ec>
2020-02-06 20:58:46 +0000

    Conexion base de datos

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb+srv://belkix:Laboratorio2020@cluster0-htw36.mongodb.net/test?retryWrites=true&w=majorityCo',
+    url: 'mongodb+srv://Pocho:Pocho2001@gya-1sxdc.gcp.mongodb.net/test?retryWrites=true&w=majority',
     //serverport: 3000 puerto para desarrollo
     serverport: 443 //puerto en producción
 }
