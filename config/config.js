52d5a185db7d322020911d68079e28f9bc7b8db3
Fernando10g97 <luis.guerra@pucese.edu.ec>
2020-02-06 23:56:23 +0000

    Modificacion de Mongo

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     url: 'mongodb+srv://Pocho:Pocho2001@gya-1sxdc.gcp.mongodb.net/test?retryWrites=true&w=majority',
-    //serverport: 3000 puerto para desarrollo
+    //serverport: 3000 puerto para desarrollo del proyecto
     serverport: 443 //puerto en producción
 }
